2ee5c7ae0bf55eec33ed986e2a7cf393e36abc15
Panthut Ketphan <panthat.k@gmail.com>
2023-11-23 10:22:57 +0700

    done login that will return person_id and their role

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 # try wrapping the code below that reads a persons.csv file in a class and make it more general such that it can read in any csv file
 
-import csv, os
+import csv
+import os
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -14,8 +15,34 @@ print(persons)
 
 # add in code for a Database class
 
+
+class DB:
+    def __init__(self):
+        self.database = []
+
+    def insert(self, table):
+        self.database.append(table)
+
+    def search(self, table_name):
+        for table in self.database:
+            if table.table_name == table_name:
+                return table
+        return None
 # add in code for a Table class
 
-# modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
 
-# modify the code in the Table class so that it supports the update operation where an entry's value associated with a key can be updated
+class Table:
+    def __init__(self, table_name, table):
+        self.table_name = table_name
+        self.table = table
+
+    # modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
+    def insert_entry(self, entry):
+        self.table.append(entry)
+    # modify the code in the Table class so that it supports the update operation where an entry's value associated with a key can be updated
+
+    def update_entry(self, key, new_value):
+        for entry in self.table:
+            if key in entry:
+                entry[key] = new_value
+                break
--- project_manage.py
+++ project_manage.py
@@ -1,61 +1,86 @@
 # import database module
+import database as db
+import csv
+import os
 
-# define a funcion called initializing
+# define a function called initializing
+my_db = db.DB()
+
 
 def initializing():
-    pass
+    # create a 'persons' table
+    persons_table = db.Table('persons', [])
 
-# here are things to do in this function:
+    # add the 'persons' table into the database
+    csv_file_path = os.path.join(os.getcwd(), 'persons.csv')
+    with open(csv_file_path, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            persons_table.insert_entry(row)
 
-    # create an object to read all csv files that will serve as a persistent state for this program
+    # create a 'login table' table
+    login_table = db.Table('login', [])
 
-    # create all the corresponding tables for those csv files
+    # add the login table into the database
+    csv_file_path = os.path.join(os.getcwd(), 'login.csv')
+    with open(csv_file_path, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            login_table.insert_entry(row)
 
-    # see the guide how many tables are needed
-
-    # add all these tables to the database
+    return persons_table, login_table
 
 
-# define a funcion called login
-
+# define a function called login
 def login():
-    pass
+    access = False
+    while not access:
+        person_id = input("Enter your username: ")
+        password = input("Enter your password: ")
+        if person_id and password != " " or "":
+            for i in range(len(login_table.table)):
+                if login_table.table[i]['username'] == person_id and login_table.table[i]['password'] == password:
+                    access = True
+                    print("Login successful!")
+                    return person_id, login_table.table[i]['role']
+        elif person_id == 'exit' or password == 'exit':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid username or password. Please try again.")
 
-# here are things to do in this function:
-   # add code that performs a login task
-        # ask a user for a username and password
-        # returns [ID, role] if valid, otherwise returning None
 
 # define a function called exit
+
+
 def exit():
     pass
 
-# here are things to do in this function:
-   # write out all the tables that have been modified to the corresponding csv files
-   # By now, you know how to read in a csv file and transform it into a list of dictionaries. For this project, you also need to know how to do the reverse, i.e., writing out to a csv file given a list of dictionaries. See the link below for a tutorial on how to do this:
-   
-   # https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
-
 
 # make calls to the initializing and login functions defined above
-
-initializing()
-val = login()
-
+person_table, login_table = initializing()
+print(login_table.table)
+person_id, role = login()
 # based on the return value for login, activate the code that performs activities according to the role defined for that person_id
 
-# if val[1] = 'admin':
+if role == 'admin':
     # see and do admin related activities
-# elif val[1] = 'student':
+    pass
+elif role == 'student':
     # see and do student related activities
-# elif val[1] = 'member':
+    pass
+elif role == 'member':
     # see and do member related activities
-# elif val[1] = 'lead':
+    pass
+elif role == 'lead':
     # see and do lead related activities
-# elif val[1] = 'faculty':
+    pass
+elif role == 'faculty':
     # see and do faculty related activities
-# elif val[1] = 'advisor':
+    pass
+elif role == 'advisor':
     # see and do advisor related activities
+    pass
 
-# once everyhthing is done, make a call to the exit function
+# once everything is done, make a call to the exit function
 exit()
