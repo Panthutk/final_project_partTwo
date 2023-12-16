368cba75f153cf105b10a1e085f5375c7c207821
Panthut Ketphan <panthat.k@gmail.com>
2023-12-16 14:45:56 +0700

    done it

--- project_manage.py
+++ project_manage.py
@@ -23,6 +23,8 @@ def initializing():
     persons_table.add_column("member2_msg", [])
     persons_table.add_column("member2", [])
     persons_table.add_column("advisor", [])
+    persons_table.add_column("advisor_status", [])
+    persons_table.add_column("advisor_msg", [])
     persons_table.add_column("status1", [])
     persons_table.add_column("status2", [])
     persons_table.add_column("submit", [])
@@ -38,29 +40,30 @@ print(login_table.table)
 
 
 def login():
-    access = False
-    while not access:
+    while True:
         person_id = input("Enter your username: ")
         password = input("Enter your password: ")
-        if person_id and password != " " or "":
-            for i in range(len(login_table.table)):
-                if login_table.table[i]['username'] == person_id and login_table.table[i]['password'] == password:
-                    access = True
-                    print("Login successful!")
-                    person_id = person_id[:-2]
-                    return person_id, login_table.table[i]['role']
-        elif person_id == 'exit' or password == 'exit':
+
+        if person_id.lower() == 'exit' or password.lower() == 'exit':
             print("Exiting...")
             break
-        else:
-            print("Invalid username or password. Please try again.")
+
+        for entry in login_table.table:
+            if entry['username'] == person_id and entry['password'] == password:
+                print("Login successful!")
+                return person_id, entry['role']
+
+        print("Invalid username or password. Please try again.")
 
 # Exit function
 
 
 def exit_program():
+    # Save the tables to the csv files
+    persons_table.save_table('persons.csv')
+    login_table.save_table('login.csv')
+
     print("Exiting...")
-    # Additional cleanup or exit logic can be added here
 
 
 # Make calls to the login function
@@ -100,7 +103,8 @@ elif role == 'lead':
     print("1. Create project")
     print("2. View project")
     print("3. Modify project")
-    print("4. Exit")
+    print("4. Send request to advisor")
+    print("6. Exit")
     while not process:
         choice = input("Enter your choice: ")
         if choice == "1":
@@ -134,6 +138,7 @@ elif role == 'lead':
                     persons_table.table[count]['member1_msg'] = project_member1_msg
                     persons_table.table[count]['member2_msg'] = project_member2_msg
                     persons_table.table[count]['type'] = "lead"
+                    login_table.table[count]['role'] = "lead"
                     print("Project created!")
                     break
         elif choice == "2":
@@ -204,10 +209,172 @@ elif role == 'lead':
                     modify_status = True
                 else:
                     print("Invalid choice. Please try again.")
+        elif choice == "4":
+            print("Sending request to advisor...")
+            for entry in persons_table.table:
+                if entry['first'] == person_id:
+                    entry['advisor_status'] = "pending"
+                    entry['status'] = "pending"
+                    print("Request sent!")
+                    break
+elif role == "student":
+    print("Welcome to the student menu!")
+    print("1. See invitation from leader")
+    print("2. Accept or Deny invitation")
+    print("3. Become leader of a project")
+    print("4. Exit")
+    process = False
+    while not process:
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            print("Viewing invitation...")
+            for entry in persons_table.table:
+                if entry['member1'] or entry["member2"] == person_id:
+                    print(entry)
+                    key = True
+        elif choice == "2" and key == True:
+            print("Accepting or denying invitation...")
+            print("1. Accept invitation")
+            print("2. Deny invitation")
+            print("3. Exit")
+            accept_status = False
+            while not accept_status:
+                accept_choice = input("Enter your choice: ")
+                if choice == 1:
+                    for entry in len(persons_table.table):
+                        if persons_table.table[entry]['member1'] == person_id:
+                            persons_table.table[entry]['status1'] = "accepted"
+                            persons_table.table[entry]['type'] = "member1"
+                            login_table.table[entry]['role'] = "member1"
+                            print("Invitation accepted!")
+                            accept_status = True
+                        elif persons_table.table[entry]['member2'] == person_id:
+                            persons_table.table[entry]['status2'] = "accepted"
+                            persons_table.table[entry]['type'] = "member2"
+                            login_table.table[entry]['role'] = "member2"
+                            print("Invitation accepted!")
+                            accept_status = True
+                    print("exit and login to see changes")
+                elif choice == 2:
+                    for entry in len(persons_table.table):
+                        if persons_table.table[entry]['member1'] == person_id:
+                            persons_table.table[entry]['status1'] = "denied"
+                            print("Invitation denied!")
+                            accept_status = True
+                        elif persons_table.table[entry]['member2'] == person_id:
+                            persons_table.table[entry]['status2'] = "denied"
+                            print("Invitation denied!")
+                            accept_status = True
 
-
-# For other roles ('member', 'lead', 'faculty', 'advisor'), add code for respective activities
-# ...
+        elif choice == "3":
+            print("Becoming leader of a project...")
+            print("1. Become leader")
+            print("2. Exit")
+            become_choice = input("Enter your choice: ")
+            become_status = False
+            while not become_status:
+                if become_choice == "1":
+                    for entry in persons_table.table:
+                        if entry['first'] == person_id:
+                            entry['type'] = "lead"
+                            print("You are now the leader!")
+                            print(
+                                "Exit and login again to see the change and used the lead menu.")
+                            become_status = True
+                elif become_choice == "2":
+                    become_status = True
+                else:
+                    print("Invalid choice. Please try again.")
+        elif choice == "4":
+            process = True
+        else:
+            print("Invalid choice. Please try again.")
+elif role == "member1" or role == "member2":
+    print("Welcome to the member menu!")
+    print("1. View project")
+    print("2. Exit")
+    process = False
+    while not process:
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            print("Viewing project...")
+            for entry in len(persons_table.table[role]):
+                if persons_table.table[entry][role] == person_id:
+                    print(persons_table.table[entry])
+                    print("Do you want to modify your project detail?")
+                    print("1. Yes")
+                    print("2. No")
+                    modify_choice = input("Enter your choice: ")
+                    modify_status = False
+                    while not modify_status:
+                        if modify_choice == "1":
+                            persons_table.table[entry]['detail'] = input(
+                                "Enter the new project detail: ")
+                            print("Project detail updated!")
+                            modify_status = True
+                        elif modify_choice == "2":
+                            modify_status = True
+                            print("Exiting...")
+                        else:
+                            print("Invalid choice. Please try again.")
+elif role == "faculty":
+    print("Welcome to the faculty menu!")
+    print("1. View all requests")
+    print("2. Add msg or approve the approve the project")
+    print("3. Exit")
+    process = False
+    while not process:
+        choice = input("Your choice: ")
+        if choice == "1":
+            print("Viewing all requests...")
+            for entry in persons_table.table["advisor_status"]:
+                if entry["advisor_status"] == "pending":
+                    print(entry)
+                    print("Do you want to accept or deny the request?")
+                    print("1. Accept")
+                    print("2. Deny")
+                    print("3. Exit")
+                    accept_status = False
+                    while not accept_status:
+                        accept_choice = input("Enter your choice: ")
+                        if accept_choice == "1":
+                            entry["advisor"] = person_id
+                            entry["advisor_status"] = "accepted"
+                            print("Request accepted!")
+                            accept_status = True
+                        elif accept_choice == "2":
+                            entry["advisor_status"] = "denied"
+                            print("Request denied!")
+                            accept_status = True
+                        elif accept_choice == "3":
+                            accept_status = True
+                        else:
+                            print("Invalid choice. Please try again.")
+        elif choice == "2":
+            select = input("Enter the project name: ")
+            for entry in persons_table.table:
+                if entry["project"] == select:
+                    print("1. Add msg")
+                    print("2. Approve the project")
+                    print("3. Exit")
+                    modify_choice = input("Enter your choice: ")
+                    modify_status = False
+                    while not modify_status:
+                        if modify_choice == "1":
+                            entry["advisor_msg"] = input(
+                                "Enter the new message: ")
+                            print("Message added!")
+                            modify_status = True
+                        elif modify_choice == "2":
+                            entry["advisor_status"] = "approved"
+                            print("Project approved!")
+                            modify_status = True
+                        elif modify_choice == "3":
+                            modify_status = True
+                        else:
+                            print("Invalid choice. Please try again.")
+        elif choice == "3":
+            process = True
 
 # Once everything is done, make a call to the exit_program function
 exit_program()
